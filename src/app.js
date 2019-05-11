29eeb1d858c05764a92968389ede04e64012a34e
Lalit Behera <lalitkumar.b@landmarkgroup.in>
2019-05-11 13:07:11 +0530

    file upload api created using the express-fileupload lib

--- src/app.js
+++ src/app.js
@@ -1,7 +1,23 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
+const upload = require('express-fileupload')
+//const http = require('http').Server(app).listen(port)
+app.use(upload())
+app.use(express.static('public'))
+app.post('/', (req, res) => {
+    if (req.files) {
+        var file = req.files.filename,
+            filename = file.name
+        file.mv(__dirname+"/../upload/" + filename, function (err) {
+            if (err) {
+                console.log(err)
+                res.send("file upload error!!")
+            } else {
+                res.send("Done!")
+            }
+        })
+    }
+})
+//console.log(__dirname);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
