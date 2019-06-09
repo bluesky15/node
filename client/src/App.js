024c0bc1db3056af9d22de4cbd6b8a6446ddba4a
lalitbehera <lalitkumar.b@landmarkgroup.in>
2019-06-09 18:30:55 +0530

    bootstrap added - component is converted to es6 style

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <form method="POST" enctype="multipart/form-data" action="/upload">
-          <label>
-            <input type="file" name="filename" />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <form method="POST" enctype="multipart/form-data" action="/upload">
+            <label>
+              <input type="file" name="filename" />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        </header>
+      </div>
+    );
+  }
 }
-
 export default App;
