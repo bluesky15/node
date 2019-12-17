a4cbcd416dc5acf42bf22013c36b09ab9916380a
lalitbehera <lalitkumar.b@landmarkgroup.in>
2019-12-18 01:09:51 +0530

    ts_: static folder added

--- server/app.ts
+++ server/app.ts
@@ -1,11 +1,13 @@
 import { Request, Response, Application } from 'express';
 import express = require('express');
 
+var path = require('path');
 var app: Application = express();
-
+app.use(express.static(path.join(__dirname+'/../../client/build')))
 const port = 3000;
 app.get('/', (req, res) => {
-  res.send('The sedulous hyena ate the antelope!');
+  //res.send('The sedulous hyena ate the antelope!');
+  res.sendFile(path.join(__dirname+'/../../client/build/index.html'))
 });
 app.listen(port, err => {
   if (err) {
--- server/dist/app.js
+++ server/dist/app.js
@@ -1,10 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
+var path = require('path');
 var app = express();
+app.use(express.static(path.join(__dirname + '/../../client/build')));
 const port = 3000;
 app.get('/', (req, res) => {
-    res.send('The sedulous hyena ate the antelope!');
+    //res.send('The sedulous hyena ate the antelope!');
+    res.sendFile(path.join(__dirname + '/../../client/build/index.html'));
 });
 app.listen(port, err => {
     if (err) {
